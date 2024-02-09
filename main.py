4cbd116899a6aca960fb11b6caf061617583b7d0
Patrick D <patrick.montejo@gmail.com>
2024-02-08 23:17:09 -0500

    added files

---
+++ main.py
@@ -0,0 +1,106 @@
+from random import choice
+
+
+def card_picker():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    random_card = choice(cards)
+    return random_card
+
+
+def add_card(card_list):
+    card_to_add = int(card_picker())
+    if card_to_add == 11 and sum(card_list) > 10:
+        card_to_add = 1
+
+    return card_to_add
+
+
+def card_check(card_list):
+    if sum(card_list) == 21:
+        print("Black Jack! You Won!")
+        return False
+    elif sum(card_list) > 21:
+        print("You went over 21. You Lose..")
+        return False
+    else:
+        return True
+
+
+def dealer_card_check(card_list):
+    while sum(card_list) < 17:
+        card_list.append(card_picker())
+    if sum(card_list) == 21:
+        print("Dealer hit Black Jack! You lose..")
+        return False
+    elif sum(card_list) > 21:
+        print(f"Dealer's card: {card_list}")
+        print("Dealer went over 21. You Won!..")
+        return False
+    else:
+        return True
+
+
+def first_cards(card_list):
+
+    for first_two_cards in range(2):
+        random_card = card_picker()
+        if random_card == 11 and sum(card_list) > 10:
+            random_card = 1
+        card_list.append(random_card)
+
+
+def compare(player_score, dealer_score):
+
+    if dealer_card_check(dealer_score):
+
+        if sum(player_score) == sum(dealer_score):
+            print(f"Dealer's cards: {dealer_score}")
+            print("It's a Tie!!!")
+        elif sum(player_score) > sum(dealer_score):
+            print(f"Dealer's cards: {dealer_score}")
+            print("You beat the dealer! You won!")
+        else:
+            print(f"Dealer's cards: {dealer_score}")
+            print("The Dealer beat you! You Lose..")
+
+
+def black_jack():
+    player_cards = []
+    dealer_cards = []
+
+    first_cards(player_cards)
+    first_cards(dealer_cards)
+
+    print(f"Your first two cards: {player_cards}")
+    print(f"Dealer's first two cards: {dealer_cards[0]}, #")
+
+    pick_card_again = True
+
+    while pick_card_again:
+
+        if not card_check(player_cards):
+            pick_card_again = False
+
+        if pick_card_again:
+
+            if input("Do you want to add another card? type 'y' for Yes and 'n' for No: ") == "y":
+                player_cards.append(add_card(player_cards))
+                print(f"Your card at hand:{player_cards}")
+                pick_card_again = card_check(player_cards)
+            else:
+                pick_card_again = False
+                compare(player_cards, dealer_cards)
+
+
+print("Welcome! Let's Play Black Jack!")
+
+
+play_again = True
+
+while play_again:
+
+    black_jack()
+
+    if input("Would you like to play again? Type 'y' for Yes and 'n' for no: ") == 'n':
+        print("Thank you for playing Black Jack!")
+        play_again = False
